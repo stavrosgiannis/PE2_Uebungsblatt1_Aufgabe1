183c43eb10a9490d8d0e3faa67bd8594c6b35095
Stav ros <hackerone1@web.de>
2020-04-29 09:30:06 +0200

    final

--- PE2_Übungsblatt1_Aufgabe1/main.c
+++ PE2_Übungsblatt1_Aufgabe1/main.c
@@ -4,65 +4,143 @@
 * @date   22 April 2020
 * @see    main.h for documenation.
 *
-* struct stack and heap program
+* struct stack_t program
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct stack_t {
+// Datenstruktur für stack_t
+typedef struct stack_t_t
+{
+	int maxsize;	// define max capacity of stack_t
+	int top;
+	float error;
 	float* items;
-	int len;
 }stack_t;
 
-stack_t* createStack() {
-	stack_t* s = calloc(1, sizeof(stack_t));
-	s->len = 0;
-	s->items = NULL;
+// Dienstprogrammfunktion zum Initialisieren von stack_t
+stack_t* createStack(int capacity)
+{
+	stack_t* s = (stack_t*)malloc(sizeof(stack_t));
+
+	s->maxsize = capacity;
+	s->top = -1;
+	s->error = 0;
+	s->items = (float*)malloc(sizeof(float) * capacity);
+
 	return s;
 }
 
-char isEmpty(stack_t* s) {
+// Dienstprogrammfunktion zum Überprüfen, ob stack_t leer ist oder nicht
+int isStackEmpty(stack_t* s)
+{
+	return s->top == -1;
 }
 
-void printStackItems(stack_t* in) {
-	if (in) {
-		printf("\tptr\t\t\tvalue\n");
-		printf("|-----------------------------------------------|\n");
-		for (int i = 0; i < in->len; ++i) {
-			printf("|\t%p\t|\t%f\t|\n", &in->items[i], in->items[i]);
-		}
-		printf("|-----------------------------------------------|%s\n");
-		printf("first item:\t%f\n", in->items[0]);
-		printf("last item:\t%f\n", in->items[in->len - 1]);
+// Dienstprogrammfunktion zum Überprüfen, ob stack_t voll ist oder nicht
+int isStackFull(stack_t* s)
+{
+	return s->top == s->maxsize - 1;
+}
+
+void getError(stack_t* s) {
+	if (s->error != 0) {
+		printf("[ERROR VAR]\t%f\n\n", s->error);
 	}
 }
 
-void push(stack_t* in, float val) {
-	if (in) {
-		in->items = realloc(in->items, (1 + in->len) * sizeof(val));
-		//printf("realloc bytes: %d\n", (1 + in->len) * sizeof(val));
-		if (in->items) {
-			in->items[in->len] = val;
-			in->len++;
-		}
+// Dienstprogrammfunktion zum Hinzufügen eines Elements x in stack_t
+void push(stack_t* s, float value)
+{
+	// Überprüfen, ob stack_t bereits voll ist. Dann würde man ein Element einfügen
+	// stack overflow Abfrage
+	if (isStackFull(s))
+	{
+		printf("\n[OverFlow]\n");
+		s->error = value;
+		getError(s);
+	}
+	else {
+		printf("[PUSH]\t%f->array[%d]\n", value, s->top + 1);
+
+		// ein Element hinzufügen und erhöhung des oberen Index's
+		s->items[++s->top] = value;
 	}
 }
 
-int main() {
-	stack_t* stack1 = createStack();
-	printf("stack1 base address:\t%p\n", stack1);
+// Dienstprogrammfunktion zum Zurückgeben des obersten Elements in einem stack_t
+float top(stack_t* s)
+{
+	// auf leerem stack_t überprüfen
+	if (!isStackEmpty(s))
+		return s->items[s->top];
+	else
+		return 0;
+}
 
-	push(stack1, 5.665415411);
-	push(stack1, 2.124545151);
-	push(stack1, 9612.51561651);
-	push(stack1, 121.21315611615161561);
+// Dienstprogrammfunktion zum pop Element aus dem stack_t
+void pop(stack_t* s)
+{
+	// check for stack_t underflow
+	if (isStackEmpty(s))
+	{
+		printf("\n[UnderFlow]\n");
+		s->error = s->items[s->top--];
+		getError(s);
+	}
+	else {
+		printf("[POP]\t%f->array[%d]\n", top(s), s->top);
 
-	printf("stack->len address:\t%p\n", &stack1->len);
-	printf("stack1->items base address:\t%p\n\n", stack1->items);
-	printStackItems(stack1);
+		// Dekrementieren Sie die Größe von stack_t um 1
+		s->items[s->top--];
+	}
+}
 
+// Visualisierung des Arrays
+void printstack_tItems(stack_t* in) {
+	if (in) {
+		printf("\tptr\t\t\tvalue\t\t\tindex\n");
+		printf("|---------------------------------------------------------------|\n");
+		for (int i = 0; i < in->maxsize; ++i) {
+			printf("|\t%p\t|\t%f\t|\t%d\t|\n", &in->items[i], in->items[i], i);
+		}
+		printf("|---------------------------------------------------------------|\n\n");
+	}
+}
+
+// main function
+int main()
+{
+	printf("\t\tProgrammentwicklung 2 - Aufgabe 1\n");
+	printf("-----------------------------------------------------------------\n\n");
+
+	// ein Stack mit der Größe 2 wird erstellt
+	stack_t* s = createStack(3);
+
+	// das Stack wird gewollt "überfüllt" um einen Overflow und Underflow zu provozieren
+	push(s, 1.5151);
+	push(s, 2);
+	push(s, 3);
+
+	printf("Top element is:\t%f\n", top(s));
+
+	pop(s);
+	pop(s);
+	pop(s);
+
+	// DEBUG analyse
+	printf("stack_t base address:\t%p\n", s);
+	printf("stack_t->items start address:\t%p\n\n", s->items);
+
+	printstack_tItems(s);
+	///////////////////////////////
+
+#ifdef _WIN32
 	getchar();
+#endif // _WIN32
+
 	return 0;
 }
